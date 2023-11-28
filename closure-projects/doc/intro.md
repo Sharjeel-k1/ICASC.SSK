524523dca4a0542a37a0ba230b7f407dff402a72
Sharjeel-k1 <87121880+Sharjeel-k1@users.noreply.github.com>
2023-11-28 22:41:36 +0100

    Add files via upload

---
+++ closure-projects/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to closure-projects
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ closure-projects/test/closure_projects/core_test.clj
@@ -0,0 +1,7 @@
+(ns closure-projects.core-test
+  (:require [clojure.test :refer :all]
+            [closure-projects.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
